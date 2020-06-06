2667d9e2ebc60db32d91c327644ae374fc088758
Harry Rose <harry@senseye.io>
2020-06-06 14:29:31 +0100

    adding termite configuration bootstrap

--- install.sh
+++ install.sh
@@ -4,5 +4,17 @@
 # Installs the dependencies for this configuration
 #
 
-yay -S swayidle swaylock slurp i3blocks mako pamix-git grim wl-clipboard
+yay -S swayidle swaylock slurp i3blocks mako pamix-git grim wl-clipboard termite
+
+termiteConfig="~/.config/termite/config"
+mkdir -P $(dirname $termiteConfig)
+
+if [[ ! -f $termiteConfig ]]
+then
+  echo "Writing termite configuration to $termiteConfig";
+  echo "[colors]" > $termiteConfig
+  echo "background=#000000" >> $termiteConfig
+else
+  echo "Termite configuration exists at $termiteConfig. Will not overwrite"
+fi
 
