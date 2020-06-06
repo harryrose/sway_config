31acee61408acb6d614af8acfaa9c92bf77357de
Harry Rose <harry@harryrose.org>
2020-06-06 16:20:08 +0100

    setting font, browser and display off timeout

--- install.sh
+++ install.sh
@@ -4,16 +4,20 @@
 # Installs the dependencies for this configuration
 #
 
-yay -S swayidle swaylock slurp i3blocks mako pamix-git grim wl-clipboard termite
+yay -S swayidle swaylock slurp i3blocks mako pamix-git grim wl-clipboard termite nerd-fonts-fira-code
 
-termiteConfig="~/.config/termite/config"
-mkdir -P $(dirname $termiteConfig)
+termiteConfig=~/.config/termite/config
+mkdir -p $(dirname $termiteConfig)
 
 if [[ ! -f $termiteConfig ]]
 then
   echo "Writing termite configuration to $termiteConfig";
   echo "[colors]" > $termiteConfig
   echo "background=#000000" >> $termiteConfig
+  echo  >> $termiteConfig
+  echo "[options]" >> $termiteConfig
+  echo "font = FiraCode Nerd Font Mono 10" >> $termiteConfig
+
 else
   echo "Termite configuration exists at $termiteConfig. Will not overwrite"
 fi
