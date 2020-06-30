1b868b300eda4b29955186779b976cdc637cbca2
Harry Rose <harry@senseye.io>
2020-06-30 13:22:58 +0100

    copying converted date before notifying

--- date-convert.sh
+++ date-convert.sh
@@ -6,8 +6,8 @@ original=`$pasteFn`
 converted=`$pasteFn | ~/.config/sway/date-convert.py`
 if [ $? -eq 0 ] 
 then
-  notify-send "Date converted" "$original is $converted"
   echo -n $converted | $copyFn
+  notify-send "Date converted" "$original is $converted"
 else
   notify-send "Unable to convert timestamp" "Clipboard does not contain a valid timestamp"
 fi
