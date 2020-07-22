fad577d520acd694152e5fee9ca4aa3697d4e74d
Harry Rose <harry@senseye.io>
2020-07-22 11:35:50 +0100

    watson bindings + improvements

---
+++ watson.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ $# < 1 ]]
+then
+  echo "Usage:"
+  echo " $0 <command>"
+  exit 1
+fi
+
+case $1 in
+  "stop")
+    watson stop || true
+    notify-send "Timer" "Timer stopped"
+    ;;
+  "start")
+    watson stop 2>&1 > /dev/null || true
+    watson projects | fzf --print-query | xargs watson start  
+    notify-send "Timer" "$(watson status -p) started"
+    ;;
+  *)
+    echo "Unknown command $1"
+    exit 2
+    ;;
+esac
